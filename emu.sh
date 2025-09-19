f0e3e37162d8521a31bca04867045bea2889d2ed
Cirrus CI <support@cirruslabs.org>
2025-09-19 20:53:42 +0000

    emu

--- emu.sh
+++ emu.sh
@@ -1,6 +1,6 @@
 cp ./build/app/outputs/flutter-apk/app-release.apk .
 
-curl -X POST https://api.appetize.io/v1/apps/b_xmb2plwjnp2gladundg4jme3pi \
+curl -X POST https://api.appetize.io/v1/apps \
   -H "X-API-KEY: ${APPETIZE_API_KEY}" \
   -F "file=@app-release.apk" \
   -F "platform=android"
