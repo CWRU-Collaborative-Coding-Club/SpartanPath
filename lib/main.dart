a1c64fb2c5f1e67b6d3d79604b3259b595687c92
Cirrus CI <support@cirruslabs.org>
2025-09-26 21:15:17 +0000

    bounded map

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:mapbox_maps_example/animated_route_example.dart';
-import 'package:mapbox_maps_example/animation_example.dart';
-import 'package:mapbox_maps_example/camera_example.dart';
-import 'package:mapbox_maps_example/circle_annotations_example.dart';
-import 'package:mapbox_maps_example/cluster_example.dart';
-import 'package:mapbox_maps_example/offline_map_example.dart';
-import 'package:mapbox_maps_example/model_layer_example.dart';
-import 'package:mapbox_maps_example/ornaments_example.dart';
-import 'package:mapbox_maps_example/geojson_line_example.dart';
-import 'package:mapbox_maps_example/image_source_example.dart';
-import 'package:mapbox_maps_example/map_interface_example.dart';
-import 'package:mapbox_maps_example/standard_style_import_example.dart';
-import 'package:mapbox_maps_example/polygon_annotations_example.dart';
-import 'package:mapbox_maps_example/polyline_annotations_example.dart';
-import 'package:mapbox_maps_example/simple_map_example.dart';
-import 'package:mapbox_maps_example/snapshotter_example.dart';
-import 'package:mapbox_maps_example/spinning_globe_example.dart';
-import 'package:mapbox_maps_example/traffic_route_line_example.dart';
-import 'package:mapbox_maps_example/tile_json_example.dart';
-import 'package:mapbox_maps_example/vector_tile_source_example.dart';
-import 'package:mapbox_maps_example/viewport_example.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-import 'full_map_example.dart';
-import 'location_example.dart';
-import 'example.dart';
-import 'point_annotations_example.dart';
-import 'projection_example.dart';
-import 'style_example.dart';
-import 'gestures_example.dart';
-import 'debug_options_example.dart';
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
 
-final List<Example> _allPages = <Example>[
-  SimpleMapExample(),
-  ViewportExample(),
-  SnapshotterExample(),
-  TrafficRouteLineExample(),
-  OfflineMapExample(),
-  ModelLayerExample(),
-  DebugOptionsExample(),
-  SpinningGlobeExample(),
-  StandardStyleImportExample(),
-  FullMapExample(),
-  StyleExample(),
-  CameraExample(),
-  ProjectionExample(),
-  MapInterfaceExample(),
-  StyleClustersExample(),
-  AnimationExample(),
-  PointAnnotationExample(),
-  CircleAnnotationExample(),
-  PolylineAnnotationExample(),
-  PolygonAnnotationExample(),
-  VectorTileSourceExample(),
-  DrawGeoJsonLineExample(),
-  ImageSourceExample(),
-  TileJsonExample(),
-  LocationExample(),
-  GesturesExample(),
-  OrnamentsExample(),
-  AnimatedRouteExample(),
-];
+class _MapScreenState extends State<MapScreen> {
+  MapboxMap? mapboxMap;
 
-class MapsDemo extends StatelessWidget {
-  // FIXME: You need to pass in your access token via the command line argument
-  // --dart-define=ACCESS_TOKEN=ADD_YOUR_TOKEN_HERE
-  // It is also possible to pass it in while running the app via an IDE by
-  // passing the same args there.
-  //
-  // Alternatively you can replace `String.fromEnvironment("ACCESS_TOKEN")`
-  // in the following line with your access token directly.
-  static const String ACCESS_TOKEN = "";
+  void _onMapCreated(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+    _restrictMapBounds();
+  }
 
+  void _restrictMapBounds() async {
+    if (mapboxMap != null) {
+      // Define the bounding box for CWRU
+      CoordinateBounds cwruBounds = CoordinateBounds(
+        southwest: Point(coordinates: Position(-81.6150, 41.5000)), // (-81.6150, 41.5000),
+        northeast: Point(coordinates: Position(-81.6045, 41.5100)),
+        infiniteBounds: false
+        
+      );
+
+      await mapboxMap!.setBounds(CameraBoundsOptions(
+        bounds: cwruBounds,
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('MapboxMaps examples')),
-      body: FullMapExample()
+      body: MapWidget(
+        onMapCreated: _onMapCreated,
+        // If needed, set the access token globally before running the app:
+      ),
     );
   }
-
-
 }
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  MapboxOptions.setAccessToken(MapsDemo.ACCESS_TOKEN);
-  runApp(MaterialApp(home: MapsDemo()));
+  MapboxOptions.setAccessToken("");
+  runApp(MaterialApp(
+    home: MapScreen(),
+  ));
 }
